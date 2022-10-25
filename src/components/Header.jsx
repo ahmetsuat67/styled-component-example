b04dfc76bc170465106c3c7689a149117e0c7e58
ahmetsuat67 <93923638+ahmetsuat67@users.noreply.github.com>
2022-10-25 16:23:19 +0300

    styled component

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import Button from "./styles/Button.styled"
+
+
+const Header = () => {
+  return (
+   <>
+    <Button>Apply Courses</Button>
+    <Button>Talk to Adviser</Button>
+   </>
+
+
+    )
+
+}
+
+export default Header
